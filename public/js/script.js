18e636207a82ee4933c40cdd5868c5756befab18
Patrik Spathon <patrik@spathon.com>
2014-10-30 23:45:33 +0100

    Display game url and add the id to the game object

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,36 @@
 'use strict';
 
 var socket = io.connect();
-var id = function(id) { return document.getElementById(id); };
-var valueById = function(_id) { return id(_id).value; };
+var $id = function(id) { return document.getElementById(id); };
+var $valueById = function(id) { return $id(id).value; };
 
 
 var Game = function() {
 
-  this.$startChallenge = id('startChallenge');
-  this.$gameOn = id('gameOn');
+  this.$startChallenge = $id('startChallenge');
+  this.$gameOn = $id('gameOn');
+  this.id = '';
 
   this.init();
 };
 
 Game.prototype.init = function() {
-  if(window.location.pathname.match('\/game\/[a-z0-9]+\/?')){
-    this.gameOn();
+  var gamePath = window.location.pathname.match('\/game\/[a-z0-9]+');
+  if(gamePath){
+    var id = gamePath[0].replace('/game/', '');
+    this.gameOn(id);
   }
-}
-
-Game.prototype.gameOn = function() {
-  this.$startChallenge.classList.toggle('hidden');
-  this.$gameOn.classList.toggle('hidden');
 };
 
+Game.prototype.gameOn = function(id) {
+  this.id = id;
+  this.$startChallenge.classList.toggle('hidden');
+  this.$gameOn.classList.toggle('hidden');
 
+  $id('gameId').value = window.location.href;
+};
 
-var theGame = new Game;
-
-
-
+var theGame = new Game();
 
 
 document.forms.startChallenge.onsubmit = function(e){
@@ -37,8 +38,8 @@ document.forms.startChallenge.onsubmit = function(e){
   e.preventDefault();
 
   var data = {
-    challenger: valueById('challenger'),
-    opponent: valueById('opponent')
+    challenger: $valueById('challenger'),
+    opponent: $valueById('opponent')
   };
 
   socket.emit('new game', data);
@@ -47,8 +48,14 @@ document.forms.startChallenge.onsubmit = function(e){
 };
 
 
+// Select all text when clicking
+var gameId = $id('gameId');
+gameId.parentNode.addEventListener('click', function(){ gameId.select(); }, false);
+
+
+
 socket.on('game on', function(id){
   // window.location = '/game/'+ id;
   window.history.pushState({ do_I: 'matter' }, 'Start game on', '/game/'+ id);
-  theGame.gameOn();
+  theGame.gameOn(id);
 });
